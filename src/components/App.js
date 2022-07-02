986fa8d937ddfeac23db9fe6be392ef9bc44fc7b
Abhishek Shrivastava <100221081+ImAbhi1@users.noreply.github.com>
2022-07-02 12:24:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+@@ -3,13 +3,14 @@ import '../styles/App.css';
 
 const App = () => {
   return (
     <div id="main"></div>
+    <h1 data-ns-test="project-namr"> Online food selling App</h1>
+    <dl data-ns-test="project-description">
+    <dd>Sellling Sea Food , fishes nad meats  online</dd>
+    <dl>
   )
 }
+    <>
+      <h1 data-ns-test="project-name"> Online Food Selling App </h1>
+      <dl data-ns-test="project-description">
+        <dd>Selling Sea food, fishes and meats in online </dd>
+      </dl>
+    </>
+  );
+};
 
 
 export default App;
