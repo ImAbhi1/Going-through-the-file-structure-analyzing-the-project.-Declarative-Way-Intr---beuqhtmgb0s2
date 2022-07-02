c37113f6c7e66f7874ced79858809e5f614fcf05
Abhishek Shrivastava <100221081+ImAbhi1@users.noreply.github.com>
2022-07-02 16:06:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@
 
 const App = () => {
   return (
+       <>
       <h1 data-ns-test="project-name"> Online Food Selling App </h1>
       <dl data-ns-test="project-description">
         <dd>Selling Sea food, fishes and meats in online </dd>
